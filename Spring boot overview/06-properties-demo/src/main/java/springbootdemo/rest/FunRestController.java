0a7d855ee57db541d2e6c000b8cc2af199d5d61a
Sait Yolal <sadoyol@hotmail.com>
2023-03-16 19:06:55 +0300

    spring-boot-overview

---
+++ Spring boot overview/06-properties-demo/src/main/java/springbootdemo/rest/FunRestController.java
@@ -0,0 +1,45 @@
+package springbootdemo.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    // inject properties for coach.name adn team.name
+    @Value("${coach.name}")
+    public String coachName;
+
+    @Value("${team.name}")
+    public String teamName;
+
+    //expose new endpoint for teaminfo
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo(){
+        return "Coach: " +coachName+", Team Name: " +teamName;
+    }
+
+
+
+    // expose " / " that return "Hello World"
+
+    @GetMapping("/")
+    public String sayHello() {
+
+        return "Hello World!";
+    }
+
+    // expose a new endpoint for "workout"
+    @GetMapping("/workout")
+    public String getdailyWorkout() {
+        return "Run a hard 5k!";
+    }
+
+    // expose a new endpoint for "fortune"
+    @GetMapping("/fortune")
+    public String getDailyFortune() {
+        return "Today is your lucky day!";
+    }
+}
